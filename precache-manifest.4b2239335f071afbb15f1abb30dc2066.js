7aa5fdcfd550d6bc4a36573432649d1d643fa95c
Federico Luna <federico.luna@tecso.coop>
2020-10-13 09:19:15 -0300

    Updates

---
+++ precache-manifest.4b2239335f071afbb15f1abb30dc2066.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eb437b5cc3ddae9111cc3b45d04a097",
+    "url": "/pic/index.html"
+  },
+  {
+    "revision": "a3c27b53c0e3b97452c1",
+    "url": "/pic/static/js/2.bb8826c9.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/pic/static/js/2.bb8826c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4cae8993f57504577a8",
+    "url": "/pic/static/js/main.c34a717d.chunk.js"
+  },
+  {
+    "revision": "2ed982e534feecb78e7b",
+    "url": "/pic/static/js/runtime-main.37bc9473.js"
+  }
+]);
